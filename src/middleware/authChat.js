b3a639ab8483f04335e45e5ea7308ac71eab6976
vincentJunior1 <liekian71@gmail.com>
2021-01-18 15:47:39 +0700

    added Feature Chatting and Auth For Chatting

---
+++ src/middleware/authChat.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const helper = require('../helper/response')
+
+module.exports = {
+  authorization: (req, res, next) => {
+    let token = req.headers.authorization
+    token = token.split(' ')[1]
+    jwt.verify(token, 'KERJAIN', (error, result) => {
+      if (
+        (error && error.name === 'JsonWebTokenError') ||
+        (error && error.name === 'TokenExpiredError')
+      ) {
+        return helper.response(res, 400, error.message)
+      } else {
+        res.token = result
+        console.log(res.token)
+        next()
+      }
+    })
+  }
+}
