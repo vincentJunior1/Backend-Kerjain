520123a8f223334a474fe05fe5ccc43d32cb553b
Risky Amalia Haris <amaliaharisr@gmail.com>
2021-01-19 02:16:35 +0700

    change query of get user

--- src/controller/c_workers.js
+++ src/controller/c_workers.js
@@ -8,7 +8,7 @@ const {
   registerUserModel,
   dataAllWorkers,
   dataByIdModel,
-  dataByCheckId,
+  // dataByCheckId,
   getUserByKeyModel,
   settingWorkersModel
 } = require('../model/m_workers')
